641f6c5ec46cce5d716120072f0ca1c48038a428
Adam Gudynowski <karambolis8@gmail.com>
2016-11-14 11:21:41 +0100

    Created unit tests

---
+++ HomepageGalleryGenerator2.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace HomepageGalleryGenerator2.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void EmptyTest()
+        { }
+    }
+}
