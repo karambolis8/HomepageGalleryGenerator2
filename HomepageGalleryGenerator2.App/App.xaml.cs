a268335148eb4127a31f1c4b2e9e51b3767f4fa1
Adam Gudynowski <karambolis8@gmail.com>
2016-11-14 10:25:20 +0100

    Layout stub

---
+++ HomepageGalleryGenerator2.App/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace HomepageGalleryGenerator2.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
